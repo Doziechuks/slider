99fdb6c873abedcbbb36a3e5de776bf6258f93ab
doziechuks <doziechuks1010@gmail.com>
2022-09-03 17:23:08 +0100

    auto slider first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import data from "./data";
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const [background, setBackground] = useState(data);
+  useEffect(()=>{
+    let lastIndex = background.length-1;
+    if(index < 0){
+      setIndex(lastIndex)
+    }
+    if(index > lastIndex){
+      setIndex(0)
+    }
+  }, [index, background])
+  useEffect(()=>{
+    let timerId = setInterval(()=>{
+      setIndex(index + 1)
+    }, 3000);
+    return ()=>{clearInterval(timerId)}
+  }, [index])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1 className="title">Image slider</h1>
+      <section className="section-container">
+        {background.map((data, dataIndex) => {
+          const { image, name, id } = data;
+          let position = "nextSlide";
+          if (dataIndex === index) {
+            position = "active";
+          }
+          if(dataIndex === index -1 || (index === 0 && dataIndex === background.length-1)){
+            position = 'prevSlide'
+          }
+          return (
+            <div className={`content ${position}`} key={id}>
+              <img className="image" src={image} alt={name} />
+            </div>
+          );
+        })}
+
+        <div className="btn-container">
+          <button onClick={() => setIndex(index - 1)} className="btn">
+            <FiChevronLeft />
+          </button>
+          <button onClick={() => setIndex(index + 1)} className="btn">
+            <FiChevronRight />
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: "kene",
+    id: 1
+  },
+  {
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "ben",
+    id: 2
+  },
+  {
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "collins",
+    id: 3
+  },
+  {
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "big",
+    id: 4
+  },
+];
+
+export default data;
